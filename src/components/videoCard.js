4bc464bb2f8abe74bfead1eb96fb9af9e8b21d48
Evar-SF27 <evarfomuso29@gmail.com>
2022-09-24 20:14:34 +0100

    update - 24/09/2022

---
+++ src/components/videoCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Typography, Card, CardContent, CardMedia } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+
+const VideoCard = ({ item }) => {
+  return (
+    <div>
+      Video Card
+    </div>
+  )
+}
+
+export default VideoCard
